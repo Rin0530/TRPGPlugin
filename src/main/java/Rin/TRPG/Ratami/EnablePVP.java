92e05e40f7d723e3583abff12e7860565226b4ee
BuildTools <unconfigured@null.spigotmc.org>
2020-09-18 21:03:26 +0900

    現在PvPが有効かを確認できるように変更

--- src/main/java/Rin/TRPG/Ratami/EnablePVP.java
+++ src/main/java/Rin/TRPG/Ratami/EnablePVP.java
@@ -12,14 +12,18 @@ public class EnablePVP implements CommandExecutor{
     @Override
     public boolean onCommand(CommandSender sender,Command command, String label,
     String[] args){
+        if(args.length == 0){
+            sender.sendMessage("PvPの有効化 "+String.valueOf(plugin.getServer().getWorld("world").getPVP()));
+            return true;
+        }
         if(args[0].equals("true")){
             plugin.getServer().getWorld("world").setPVP(true);
         }
-            else if(args[0].equals("false")){
-            plugin.getServer().getWorld("world").setPVP(false);
-            }
-            else
-            sender.sendMessage("オプションにはtrueもしくはfalseを入れてください");
+        else if(args[0].equals("false")){
+        plugin.getServer().getWorld("world").setPVP(false);
+        }
+        else
+        sender.sendMessage("オプションにはtrueもしくはfalseを入れてください");
         return true;
     }
 }
